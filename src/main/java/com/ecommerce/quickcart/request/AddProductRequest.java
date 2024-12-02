a6d750f85c791e1a2ea28649217d53354777bee5
Ninad More <ninadmore183@gmail.com>
2024-12-02 19:45:53 +0530

    Comments added for better understanding

--- src/main/java/com/ecommerce/quickcart/request/AddProductRequest.java
+++ src/main/java/com/ecommerce/quickcart/request/AddProductRequest.java
@@ -6,6 +6,8 @@ import com.ecommerce.quickcart.model.Category;
 
 import lombok.Data;
 
+// Specifies the request that we want to pass to the function from the frontend
+
 @Data
 public class AddProductRequest {
     private Long id;
--- src/main/java/com/ecommerce/quickcart/response/ApiResponse.java
+++ src/main/java/com/ecommerce/quickcart/response/ApiResponse.java
@@ -3,6 +3,8 @@ package com.ecommerce.quickcart.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+// Specifies the respose thats is returned to the frontend from the APIs 
+
 @Data
 @AllArgsConstructor
 public class ApiResponse {
