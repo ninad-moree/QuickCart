205fbc9b73d9bd423ac116732c12dfde03ba47f5
Ninad More <ninadmore183@gmail.com>
2024-12-03 21:18:10 +0530

    CartItem basic utility functions created

---
+++ src/main/java/com/ecommerce/quickcart/service/cart/CartItemService.java
@@ -0,0 +1,92 @@
+package com.ecommerce.quickcart.service.cart;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.quickcart.exceptions.ResourceNotFoundException;
+import com.ecommerce.quickcart.model.Cart;
+import com.ecommerce.quickcart.model.CartItem;
+import com.ecommerce.quickcart.model.Product;
+import com.ecommerce.quickcart.repository.CartItemRepository;
+import com.ecommerce.quickcart.repository.CartRepository;
+import com.ecommerce.quickcart.service.product.IProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CartItemService implements ICartItemService {
+    private final CartItemRepository cartItemRepository;
+    private final CartRepository cartRepository;
+    private final IProductService productService;
+    private final ICartService cartService;
+
+    @Override
+    public void addItemToCart(Long cartId, Long productId, int quantity) {
+        // 1. Get the cart
+        Cart cart = cartService.getCart(cartId);
+        // 2. Get the product
+        Product product = productService.getProductById(productId);
+
+        // 3. Check if the product is already in the cart
+        CartItem cartItem = cart.getCartItems()
+                                .stream()
+                                .filter(item -> item.getProduct().getId().equals(productId))
+                                .findFirst().orElse(new CartItem());
+                                
+        // 4. If Yes --> add the product to the cart
+        if(cartItem.getId() == null) {
+            cartItem.setCart(cart);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(quantity);
+            cartItem.setUnitPrice(product.getPrice());
+        } // 5. If No ---> increase the quantity of the product
+        else { 
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        }
+
+        cartItem.setTotalPrice();
+        cart.addItem(cartItem);
+
+        cartItemRepository.save(cartItem);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void removeItemFromCart(Long cartId, Long productId) {
+        Cart cart = cartService.getCart(cartId);
+        CartItem itemToRemove = getCartItem(cartId, productId);
+        cart.removeItem(itemToRemove);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        cart.getCartItems()
+            .stream()
+            .filter(item -> item.getProduct().getId().equals(productId))
+            .findFirst()
+            .ifPresent(item -> {
+                item.setQuantity(quantity);
+                item.setUnitPrice(item.getProduct().getPrice());
+                item.setTotalPrice();
+            });
+        
+            BigDecimal totalAmount = cart.getTotalAmount();
+            cart.setTotalAmount(totalAmount);
+            cartRepository.save(cart);
+    }
+
+    @Override
+    public CartItem getCartItem(Long cartId, Long productId) {
+        Cart cart = cartService.getCart(cartId);
+        return cart.getCartItems()
+                    .stream()
+                    .filter(item -> item.getProduct().getId().equals(productId))
+                    .findFirst()
+                    .orElseThrow(() -> new ResourceNotFoundException("Item not found"));
+    }
+
+}
