5219423dd5365c7339a14e13aa48429a1f2dd8fc
Ninad More <ninadmore183@gmail.com>
2024-12-04 20:12:16 +0530

    Cart Item APis created

--- src/main/java/com/ecommerce/quickcart/service/cart/CartService.java
+++ src/main/java/com/ecommerce/quickcart/service/cart/CartService.java
@@ -1,6 +1,7 @@
 package com.ecommerce.quickcart.service.cart;
 
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 public class CartService implements ICartService  {
     private final CartRepository cartRepository;
     private final CartItemRepository cartItemRepository;
+    private final AtomicLong cartIdGenerator = new AtomicLong(0);
 
     @Override
     public Cart getCart(Long id) {
@@ -39,6 +41,14 @@ public class CartService implements ICartService  {
     public BigDecimal getTotalPrice(Long id) {
         Cart cart = getCart(id);
         return cart.getTotalAmount();
+    }
+
+    @Override
+    public Long inititalizeNewCart() {
+        Cart newCart = new Cart();
+        Long newCartId = cartIdGenerator.incrementAndGet();
+        newCart.setId(newCartId);
+        return cartRepository.save(newCart).getId();
     }
 
 }
