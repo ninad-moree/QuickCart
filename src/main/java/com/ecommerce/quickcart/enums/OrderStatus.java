d4d786bd3b4f34dc2d5b21c2a85d25ec5df45a3f
Ninad More <ninadmore183@gmail.com>
2024-12-15 19:06:09 +0530

    Order and OrderItem model created

---
+++ src/main/java/com/ecommerce/quickcart/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.ecommerce.quickcart.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ src/main/java/com/ecommerce/quickcart/model/Order.java
@@ -0,0 +1,40 @@
+package com.ecommerce.quickcart.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.ecommerce.quickcart.enums.OrderStatus;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    private LocalDate orderDate;
+
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderItem> orderItems = new HashSet<>();
+}
